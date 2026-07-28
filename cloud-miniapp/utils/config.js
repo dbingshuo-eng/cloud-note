b790b52fd2f8fe70f478b37b3a7f2bd326464b61
xiaozhu <dbingshuo@gmail.com>
2026-07-28 19:28:23 +0800

    Change SERVER_URL to localhost and add comments
    Updated SERVER_URL for local development and added comments for clarity.

--- cloud-miniapp/utils/config.js
+++ cloud-miniapp/utils/config.js
@@ -1,4 +1,8 @@
-const SERVER_URL = 'http://192.168.1.9:8080';
+// 本机微信开发者工具调试地址。
+// 真机调试时，改成电脑局域网 IP，例如：http://192.168.x.x:8080
+// 正式发布时，改成已备案的 HTTPS 域名。
+const SERVER_URL = 'http://127.0.0.1:8080';
+
 const TOKEN_KEY = 'cloudDiskToken';
 const USER_KEY = 'cloudDiskUser';
 
